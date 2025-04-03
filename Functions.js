f44d1222359bd7add05a9eecedd744bfa94e18db
D0myyy <201152420+D0myyy@users.noreply.github.com>
2025-04-03 21:38:03 +0300

    "AI" Opponent, made bot better / block or win if the chance is there!

--- Functions.js
+++ Functions.js
@@ -10,6 +10,8 @@ const board = {
     '9': "",
 };
 
+let move = 0;
+
 const buttons = document.getElementById("Buttons").children;
 
 const title = document.getElementById("title");
@@ -24,107 +26,344 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function checkWinX(){
-    if(board[1] == 'X' && board[2] == 'X' && board[3] == 'X'){
+function if3Win() {
+    if (board[1] == '' && board[2] == 'O' && board[3] == 'O') {
+        return 1;
+    } else if (board[1] == 'O' && board[2] == '' && board[3] == 'O') {
+        return 2;
+    } else if (board[1] == 'O' && board[2] == 'O' && board[3] == '') {
+        return 3;
+    }
+
+    if (board[4] == '' && board[5] == 'O' && board[6] == 'O') {
+        return 4;
+    } else if (board[4] == 'O' && board[5] == '' && board[6] == 'O') {
+        return 5;
+    } else if (board[4] == 'O' && board[5] == 'O' && board[6] == '') {
+        return 6;
+    }
+
+    if (board[7] == '' && board[8] == 'O' && board[9] == 'O') {
+        return 7;
+    } else if (board[7] == 'O' && board[8] == '' && board[9] == 'O') {
+        return 8;
+    } else if (board[7] == 'O' && board[8] == 'O' && board[9] == '') {
+        return 9;
+    }
+
+    if (board[1] == '' && board[4] == 'O' && board[7] == 'O') {
+        return 1;
+    } else if (board[1] == 'O' && board[4] == '' && board[7] == 'O') {
+        return 4;
+    } else if (board[1] == 'O' && board[4] == 'O' && board[7] == '') {
+        return 7;
+    }
+
+    if (board[2] == '' && board[5] == 'O' && board[8] == 'O') {
+        return 2;
+    } else if (board[2] == 'O' && board[5] == '' && board[8] == 'O') {
+        return 5;
+    } else if (board[2] == 'O' && board[5] == 'O' && board[8] == '') {
+        return 8;
+    }
+
+    if (board[3] == '' && board[6] == 'O' && board[9] == 'O') {
+        return 3;
+    } else if (board[3] == 'O' && board[6] == '' && board[9] == 'O') {
+        return 6;
+    } else if (board[3] == 'O' && board[6] == 'O' && board[9] == '') {
+        return 9;
+    }
+
+    if (board[1] == '' && board[5] == 'O' && board[9] == 'O') {
+        return 1;
+    } else if (board[1] == 'O' && board[5] == '' && board[9] == 'O') {
+        return 5;
+    } else if (board[1] == 'O' && board[5] == 'O' && board[9] == '') {
+        return 9;
+    }
+
+    if (board[3] == '' && board[5] == 'O' && board[7] == 'O') {
+        return 3;
+    } else if (board[3] == 'O' && board[5] == '' && board[7] == 'O') {
+        return 5;
+    } else if (board[3] == 'O' && board[5] == 'O' && board[7] == '') {
+        return 7;
+    }
+    return 0;
+}
+
+function if3Block() {
+    if (board[1] == '' && board[2] == 'X' && board[3] == 'X') {
+        return 1;
+    } else if (board[1] == 'X' && board[2] == '' && board[3] == 'X') {
+        return 2;
+    } else if (board[1] == 'X' && board[2] == 'X' && board[3] == '') {
+        return 3;
+    }
+
+    if (board[4] == '' && board[5] == 'X' && board[6] == 'X') {
+        return 4;
+    } else if (board[4] == 'X' && board[5] == '' && board[6] == 'X') {
+        return 5;
+    } else if (board[4] == 'X' && board[5] == 'X' && board[6] == '') {
+        return 6;
+    }
+
+    if (board[7] == '' && board[8] == 'X' && board[9] == 'X') {
+        return 7;
+    } else if (board[7] == 'X' && board[8] == '' && board[9] == 'X') {
+        return 8;
+    } else if (board[7] == 'X' && board[8] == 'X' && board[9] == '') {
+        return 9;
+    }
+
+    if (board[1] == '' && board[4] == 'X' && board[7] == 'X') {
+        return 1;
+    } else if (board[1] == 'X' && board[4] == '' && board[7] == 'X') {
+        return 4;
+    } else if (board[1] == 'X' && board[4] == 'X' && board[7] == '') {
+        return 7;
+    }
+
+    if (board[2] == '' && board[5] == 'X' && board[8] == 'X') {
+        return 2;
+    } else if (board[2] == 'X' && board[5] == '' && board[8] == 'X') {
+        return 5;
+    } else if (board[2] == 'X' && board[5] == 'X' && board[8] == '') {
+        return 8;
+    }
+
+    if (board[3] == '' && board[6] == 'X' && board[9] == 'X') {
+        return 3;
+    } else if (board[3] == 'X' && board[6] == '' && board[9] == 'X') {
+        return 6;
+    } else if (board[3] == 'X' && board[6] == 'X' && board[9] == '') {
+        return 9;
+    }
+
+    if (board[1] == '' && board[5] == 'X' && board[9] == 'X') {
+        return 1;
+    } else if (board[1] == 'X' && board[5] == '' && board[9] == 'X') {
+        return 5;
+    } else if (board[1] == 'X' && board[5] == 'X' && board[9] == '') {
+        return 9;
+    }
+
+    if (board[3] == '' && board[5] == 'X' && board[7] == 'X') {
+        return 3;
+    } else if (board[3] == 'X' && board[5] == '' && board[7] == 'X') {
+        return 5;
+    } else if (board[3] == 'X' && board[5] == 'X' && board[7] == '') {
+        return 7;
+    }
+    return 0;
+}
+
+function sensitiveCases() {
+    if (board[5] == 'X') {
+        x = Math.floor(Math.random() * 4);
+        while (x == 0) {
+            x = Math.floor(Math.random() * 10);
+        }
+        if (x == 1) {
+            return 1;
+        } else if (x == 2) {
+            return 3;
+        } else if (x == 3) {
+            return 7;
+        } else if (x == 4) {
+            return 9;
+        }
+    } else if (board[5] != 'X') {
+        return 5;
+    }
+    return 0;
+}
+
+function checkWinX() {
+    if (board[1] == 'X' && board[2] == 'X' && board[3] == 'X') {
         return true;
-    } else if(board[4] == 'X' && board[5] == 'X' && board[6] == 'X'){
+    } else if (board[4] == 'X' && board[5] == 'X' && board[6] == 'X') {
         return true;
-    } else if(board[7] == 'X' && board[8] == 'X' && board[9] == 'X'){
+    } else if (board[7] == 'X' && board[8] == 'X' && board[9] == 'X') {
         return true;
-    } else if(board[1] == 'X' && board[4] == 'X' && board[7] == 'X'){
+    } else if (board[1] == 'X' && board[4] == 'X' && board[7] == 'X') {
         return true;
-    } else if(board[2] == 'X' && board[5] == 'X' && board[8] == 'X'){
+    } else if (board[2] == 'X' && board[5] == 'X' && board[8] == 'X') {
         return true;
-    } else if(board[3] == 'X' && board[6] == 'X' && board[9] == 'X'){
+    } else if (board[3] == 'X' && board[6] == 'X' && board[9] == 'X') {
         return true;
-    } else if(board[1] == 'X' && board[5] == 'X' && board[9] == 'X'){
+    } else if (board[1] == 'X' && board[5] == 'X' && board[9] == 'X') {
         return true;
-    } else if(board[3] == 'X' && board[5] == 'X' && board[7] == 'X'){
+    } else if (board[3] == 'X' && board[5] == 'X' && board[7] == 'X') {
         return true;
     }
     return false;
 }
 
-function checkWinO(){
-    if(board[1] == 'O' && board[2] == 'O' && board[3] == 'O'){
+function checkWinO() {
+    if (board[1] == 'O' && board[2] == 'O' && board[3] == 'O') {
         return true;
-    } else if(board[4] == 'O' && board[5] == 'O' && board[6] == 'O'){
+    } else if (board[4] == 'O' && board[5] == 'O' && board[6] == 'O') {
         return true;
-    } else if(board[7] == 'O' && board[8] == 'O' && board[9] == 'O'){
+    } else if (board[7] == 'O' && board[8] == 'O' && board[9] == 'O') {
         return true;
-    } else if(board[1] == 'O' && board[4] == 'O' && board[7] == 'O'){
+    } else if (board[1] == 'O' && board[4] == 'O' && board[7] == 'O') {
         return true;
-    } else if(board[2] == 'O' && board[5] == 'O' && board[8] == 'O'){
+    } else if (board[2] == 'O' && board[5] == 'O' && board[8] == 'O') {
         return true;
-    } else if(board[3] == 'O' && board[6] == 'O' && board[9] == 'O'){
+    } else if (board[3] == 'O' && board[6] == 'O' && board[9] == 'O') {
         return true;
-    } else if(board[1] == 'O' && board[5] == 'O' && board[9] == 'O'){
+    } else if (board[1] == 'O' && board[5] == 'O' && board[9] == 'O') {
         return true;
-    } else if(board[3] == 'O' && board[5] == 'O' && board[7] == 'O'){
+    } else if (board[3] == 'O' && board[5] == 'O' && board[7] == 'O') {
         return true;
     }
     return false;
 }
 
-async function randomNum(){
+async function randomNum() {
     for (let y in board) {
-        if(board[y] == ""){
+        if (board[y] == "") {
             break;
         }
-        if(y == 9 && board[y] != ""){
+        if (y == 9 && board[y] != "") {
             return false;
         }
     }
 
-    let x = Math.floor(Math.random() * 10);
-    while(x==0){
-        x = Math.floor(Math.random() * 10);
-    }
-
-    if(board[x] != 'X' && board[x] != 'O'){
+    let x = sensitiveCases();
+    if (move == 1 && x != 0) {
         console.log(x)
-        document.getElementById(x).innerHTML='O';
+        document.getElementById(x).innerHTML = 'O';
         board[x] = 'O';
-        if(checkWinO()){
+        if (checkWinO()) {
             console.log("game ended");
             console.log("O won!");
             ended = true;
             title.style.opacity = "100";
             title.innerHTML = "O won!";
+            move = 0;
 
             await sleep(timeAfterRound * 1000);
-            for (let x in board){
+            for (let x in board) {
                 board[x] = "";
             }
-            for (let y in buttons){
+            for (let y in buttons) {
                 buttons[y].innerHTML = "";
             }
             ended = false;
             title.style.opacity = "0";
             return;
         }
-    } else{
-        randomNum();
+    } else {
+        x = if3Win();
+        if (x != 0) {
+            console.log(x)
+            document.getElementById(x).innerHTML = 'O';
+            board[x] = 'O';
+            if (checkWinO()) {
+                console.log("game ended");
+                console.log("O won!");
+                ended = true;
+                title.style.opacity = "100";
+                title.innerHTML = "O won!";
+                move = 0;
+
+                await sleep(timeAfterRound * 1000);
+                for (let x in board) {
+                    board[x] = "";
+                }
+                for (let y in buttons) {
+                    buttons[y].innerHTML = "";
+                }
+                ended = false;
+                title.style.opacity = "0";
+                return;
+            }
+        } else {
+
+            x = if3Block();
+            if (x != 0) {
+                console.log(x)
+                document.getElementById(x).innerHTML = 'O';
+                board[x] = 'O';
+                if (checkWinO()) {
+                    console.log("game ended");
+                    console.log("O won!");
+                    ended = true;
+                    title.style.opacity = "100";
+                    title.innerHTML = "O won!";
+                    move = 0;
+
+                    await sleep(timeAfterRound * 1000);
+                    for (let x in board) {
+                        board[x] = "";
+                    }
+                    for (let y in buttons) {
+                        buttons[y].innerHTML = "";
+                    }
+                    ended = false;
+                    title.style.opacity = "0";
+                    return;
+                }
+            } else {
+                x = Math.floor(Math.random() * 10);
+                while (x == 0) {
+                    x = Math.floor(Math.random() * 10);
+                }
+
+                if (board[x] != 'X' && board[x] != 'O') {
+                    console.log(x)
+                    document.getElementById(x).innerHTML = 'O';
+                    board[x] = 'O';
+                    if (checkWinO()) {
+                        console.log("game ended");
+                        console.log("O won!");
+                        ended = true;
+                        title.style.opacity = "100";
+                        title.innerHTML = "O won!";
+                        move = 0;
+
+                        await sleep(timeAfterRound * 1000);
+                        for (let x in board) {
+                            board[x] = "";
+                        }
+                        for (let y in buttons) {
+                            buttons[y].innerHTML = "";
+                        }
+                        ended = false;
+                        title.style.opacity = "0";
+                        return;
+                    }
+                } else {
+                    randomNum();
+                }
+            }
+        }
     }
 }
 
-async function display(input){
-    if(board[input] != 'X' && board[input] != 'O' && !ended){
-        document.getElementById(input).innerHTML='X';
+async function display(input) {
+    if (board[input] != 'X' && board[input] != 'O' && !ended) {
+        document.getElementById(input).innerHTML = 'X';
         board[input] = 'X';
+        move++;
 
-        if(checkWinX()){
+        if (checkWinX()) {
             console.log("game ended");
             console.log("X won");
             ended = true;
             title.style.opacity = "100";
             title.innerHTML = "X won!";
-            
+            move = 0;
+
             await sleep(timeAfterRound * 1000);
-            for (let x in board){
+            for (let x in board) {
                 board[x] = "";
             }
-            for (let y in buttons){
+            for (let y in buttons) {
                 buttons[y].innerHTML = "";
             }
             ended = false;
@@ -133,31 +372,34 @@ async function display(input){
         }
 
         for (let y in board) {
-            if(board[y] == ""){
+            if (board[y] == "") {
                 break;
             }
-            if(y == 9 && board[y] != ""){
+            if (y == 9 && board[y] != "") {
 
                 console.log("game ended");
                 ended = true;
-                if(checkWinX()){
+                if (checkWinX()) {
                     console.log("X won");
                     title.style.opacity = "100";
                     title.innerHTML = "X won!";
-                } else if(checkWinO()){
+                    move = 0;
+                } else if (checkWinO()) {
                     console.log("O won");
                     title.style.opacity = "100";
                     title.innerHTML = "O won!";
-                } else{
+                    move = 0;
+                } else {
                     console.log("Draw");
                     title.style.opacity = "100";
                     title.innerHTML = "Draw!";
-                    
+                    move = 0;
+
                     await sleep(timeAfterRound * 1000);
-                    for (let x in board){
+                    for (let x in board) {
                         board[x] = "";
                     }
-                    for (let y in buttons){
+                    for (let y in buttons) {
                         buttons[y].innerHTML = "";
                     }
                     ended = false;
@@ -169,5 +411,4 @@ async function display(input){
 
         randomNum();
     }
-    
 }
